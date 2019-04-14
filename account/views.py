c784e90bd8461bbcbfb07b1a9b2605a9328f8138
Kennedy Njeri <mistakenz123@gmail.com>
2019-04-15 00:02:12 +0300

    add celery

--- account/views.py
+++ account/views.py
@@ -95,7 +95,7 @@ def activate(request, uidb64, token):
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
 
 
-        #return redirect('login')
+        return redirect('login')
 
     else:
 
